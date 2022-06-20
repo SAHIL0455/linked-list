f2e9ebae4ff4cedff5e62c653dce270da098ef0e
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-20 21:09:55 +0530

    Added: Create Simple Linked list of 56,30,70

--- src/com/bridgelab/LinkedList.java
+++ src/com/bridgelab/LinkedList.java
@@ -3,5 +3,10 @@ package com.bridgelab;
 public class LinkedList {
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List Problems");
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
     }
 }
